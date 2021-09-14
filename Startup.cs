d5bee29c61a6c41ac97404424a54dd9c4e55de3b
Thaina Biudes <thaina.biudes@gmail.com>
2021-09-13 22:18:39 -0300

    feat: configura politica de cors

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(
+                options => {
+                    options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin());
+                }
+            );
             services.AddDbContext<DataContext>(
                 options => options.UseInMemoryDatabase("database")
             );
@@ -47,6 +52,8 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
