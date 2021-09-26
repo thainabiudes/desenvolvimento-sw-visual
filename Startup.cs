66d5e5760fd77f295eeb58da166faa2e9be222b1
Thaina Biudes <thaina.biudes@gmail.com>
2021-09-26 17:12:30 -0300

    feat: cria controller de atualizar produtos e cors liberado

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -12,7 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -28,11 +28,19 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Configuração para habilitar as requisições de outras origens - CORS
             services.AddCors(
-                options => {
-                    options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin());
-                }
+                options =>
+
+                    options.AddPolicy("CorsPolicy", builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                    )
+
             );
+
+            //Configuração do serviço de banco de dados
             services.AddDbContext<DataContext>(
                 options => options.UseInMemoryDatabase("database")
             );
